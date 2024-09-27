06a7c3b42989c46e73a711843b4f87c9db8a94f9
adit-bala <aditbala@berkeley.edu>
2024-09-27 00:39:59 -0700

    vnet + subnet created

--- src/tools/benchmarking/azure/azure_vm_setup.py
+++ src/tools/benchmarking/azure/azure_vm_setup.py
@@ -4,43 +4,117 @@ import os
 from dotenv import load_dotenv
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.compute.models import ProximityPlacementGroup
 from azure.mgmt.network import NetworkManagementClient
-from azure.mgmt.network.models import ProximityPlacementGroup
 from azure.mgmt.resource import ResourceManagementClient
 
 load_dotenv()
-# Load paramaters
-with open('config/vm_parameters.json') as f:
-    vm_params = json.load(f)
-with open('config/network_parameters.json') as f:
-    network_params = json.load(f)
-subscription_id = os.getenv('SUBSCRIPTION_ID')
-print(subscription_id, "hello")
-# Initialize clients
-resource_client = ResourceManagementClient(credential=DefaultAzureCredential(), subscription_id=subscription_id)
-compute_client = ComputeManagementClient(credential=DefaultAzureCredential(), subscription_id=subscription_id)
-network_client = NetworkManagementClient(credential=DefaultAzureCredential(), subscription_id=subscription_id)
+SUBSCRIPTION_ID = os.getenv('SUBSCRIPTION_ID')# Initialize clients
 
-RESOURCE_GROUP_NAME = 'rollbaccine_network'
+resource_client = ResourceManagementClient(credential=DefaultAzureCredential(), subscription_id=SUBSCRIPTION_ID)
+compute_client = ComputeManagementClient(credential=DefaultAzureCredential(), subscription_id=SUBSCRIPTION_ID)
+network_client = NetworkManagementClient(credential=DefaultAzureCredential(), subscription_id=SUBSCRIPTION_ID)
+
+RESOURCE_GROUP_NAME = 'rollbaccine_adit'
 PPG_NAME = 'rollbaccine_placement_group'
 USERNAME = 'admin'
 VM_SIZE = 'Standard_DC16ads_v5'
-NUM_VMS = 2
-LOCATION = vm_params['location']
+NUM_VMS = 3
+LOCATION = "northeurope"
+VNET_NAME = f"{RESOURCE_GROUP_NAME}_vnet"
+SUBNET_NAME = f"{RESOURCE_GROUP_NAME}_subnet"
+INTERFACE_NAME = f"{RESOURCE_GROUP_NAME}_interface"
 
+# Load paramaters
+vm_params = {
+    "location": LOCATION,
+    "hardware_profile": {
+        "vm_size": VM_SIZE
+    },
+    "storage_profile": {
+        "image_reference": {
+            "publisher": "Canonical",
+            "offer": "UbuntuServer",
+            "sku": "24_04-lts",
+            "version": "latest"
+        }
+    },
+    "os_profile": {
+        "admin_username": "rollbaccine",
+        "linux_configuration": {
+            "disable_password_authentication": True,
+            "ssh": {
+                "public_keys": [{
+                    "path": f"/home/{USERNAME}/.ssh/authorized_keys",
+                    "key_data": f"{os.getenv('SSH_KEY')}"
+                }]
+            }
+        }
+    }
+}
+
+# Create resource group
 resource_client.resource_groups.create_or_update(RESOURCE_GROUP_NAME, {'location': LOCATION})
 
+# Create PPG
 ppg_params = ProximityPlacementGroup(location=LOCATION, proximity_placement_group_type='Standard')
-network_client.proximity_placement_groups.begin_create_or_update(RESOURCE_GROUP_NAME, PPG_NAME, ppg_params)
+compute_client.proximity_placement_groups.create_or_update(RESOURCE_GROUP_NAME, PPG_NAME, ppg_params)
 
-# # Create VMs
+# Create vnet
+vnet_creation = network_client.virtual_networks.begin_create_or_update(
+    RESOURCE_GROUP_NAME,
+    VNET_NAME,
+    {
+        'location': LOCATION,
+        'address_space': {
+            'address_prefixes': ['10.0.0.0/16']
+        }
+    }
+).result()
+
+# Create subnet
+subnet_creation = network_client.subnets.begin_create_or_update(
+    RESOURCE_GROUP_NAME,
+    VNET_NAME,
+    SUBNET_NAME,
+    {
+        'address_prefix': '10.0.0.0/24'
+    }
+).result()
+
+# Create Network Interface
+network_client.network_interfaces.begin_create_or_update(
+    RESOURCE_GROUP_NAME,
+    INTERFACE_NAME,
+    {
+        'location': LOCATION,
+        'ip_configurations': [{
+            'name': 'ip_config',
+            'subnet': {
+                'id': subnet_creation.id
+            }
+        }]
+    } 
+).result()
+
+# Create VMs
 # for i in range(NUM_VMS):
 #     vm_name = f"vm_{i}"
 #     vm_parameters = vm_params.copy()
 #     vm_parameters['os_profile']['computer_name'] = vm_name
-#     vm_parameters['network_profile'] = {
-#         'network_interfaces': [{'id': subnet_id}]
+#     # Create a network interface (NIC) for each VM
+#     nic_name = f"{vm_name}_nic"
+#     nic_params = {
+#         'location': LOCATION,
+#         'ip_configurations': [{
+#             'name': f'{nic_name}_ip_config',
+#             'subnet': {
+#                 'id': f"/subscriptions/{SUBSCRIPTION_ID}/resourceGroups/{RESOURCE_GROUP_NAME}/providers/Microsoft.Network/virtualNetworks/my-vnet/subnets/default"  # Replace with your VNet ID
+#             }
+#         }]
 #     }
-#     compute_client.virtual_machines.begin_create_or_update(resource_group_name, vm_name, vm_parameters).result()
+    
+#     network_client.network_interfaces.begin_create_or_update(RESOURCE_GROUP_NAME, nic_name, nic_params)
+#     network_interface_ids.append(f"/subscriptions/{SUBSCRIPTION_ID}/resourceGroups/{RESOURCE_GROUP_NAME}/providers/Microsoft.Network/networkInterfaces/{nic_name}")
 
 # Run setup script on VM using SSH and Paramiko
