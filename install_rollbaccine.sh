fde75f076e4179fd274c239786b758cc625e7835
adit-bala <aditbala@berkeley.edu>
2024-10-04 11:58:24 -0700

    wait for vms to start before ssh

--- install_rollbaccine.sh
+++ install_rollbaccine.sh
@@ -5,6 +5,3 @@ sudo apt-get install -y build-essential
 git clone https://github.com/davidchuyaya/rollbaccine
 cd rollbaccine/src
 make
-
-# Unmount the default disk
-sudo umount /dev/sdb1
--- src/tools/benchmarking/azure/ssh_azure_vm.py
+++ src/tools/benchmarking/azure/ssh_azure_vm.py
@@ -2,6 +2,10 @@ import paramiko
 import json
 import os
 from dotenv import load_dotenv
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.compute import ComputeManagementClient
+from time import sleep
+
 
 
 load_dotenv()
@@ -21,10 +25,34 @@ for key, value in config.items():
 
 print(config)
 
+SUBSCRIPTION_ID = config['subscription_id']
 USERNAME = config['username']
 PRIVATE_KEY_PATH = config['ssh_key_path']
 SCRIPT_PATH = os.path.join(os.getenv('BASE_PATH'), config['install_script_path'])
+RESOURCE_GROUP_NAME = config['resource_group_name']
 
+compute_client = ComputeManagementClient(credential=DefaultAzureCredential(), subscription_id=SUBSCRIPTION_ID)
+
+vm_ip_data = {}
+# Save the data to a JSON file
+with open('vm_ips.json') as f:
+    vm_ip_data = json.load(f)
+
+def start_vm(vm_name):
+    print(f"Starting VM: {vm_name}")
+    start_operation = compute_client.virtual_machines.begin_start(RESOURCE_GROUP_NAME, vm_name)
+    start_operation.wait()
+
+    # Poll the VM status until it is running
+    while True:
+        vm = compute_client.virtual_machines.get(RESOURCE_GROUP_NAME, vm_name, expand='instanceView')
+        statuses = vm.instance_view.statuses
+        for status in statuses:
+            if status.code == 'PowerState/running':
+                print(f"VM {vm_name} is now running.")
+                return
+        print(f"Waiting for VM {vm_name} to start...")
+        sleep(10)  # Wait 10 seconds before checking again
 
 def ssh_and_execute(public_ip, username, private_key_path, script_path, is_leader):
     ssh = paramiko.SSHClient()
@@ -53,11 +81,14 @@ def ssh_and_execute(public_ip, username, private_key_path, script_path, is_leade
             ssh.exec_command(f'chmod +x {remote_script_path}')
             print(f"Running install script on {public_ip}")
             stdin, stdout, stderr = ssh.exec_command(f'sudo {remote_script_path}')
-            print(stdout.read().decode())
+            for line in stdout.read().splitlines():
+                print(line)
 
+        # Run leader/follower-specific commands
         if is_leader:
             print(f"Running leader-specific commands on {public_ip}")
             commands = [
+                "sudo umount /dev/sdb1",
                 "cd rollbaccine/src",
                 "sudo insmod rollbaccine.ko",
                 f"echo \"0 $(sudo blockdev --getsz /dev/sdb1) rollbaccine /dev/sdb1 1 2 0 true 250000 abcdefghijklmnop 12340\" | sudo dmsetup create rollbaccine1"
@@ -65,6 +96,7 @@ def ssh_and_execute(public_ip, username, private_key_path, script_path, is_leade
         else:
             print(f"Running follower-specific commands on {public_ip}")
             commands = [
+                "sudo umount /dev/sdb1",
                 "cd rollbaccine/src",
                 "sudo insmod rollbaccine.ko",
                 f"echo \"0 $(sudo blockdev --getsz /dev/sdb1) rollbaccine /dev/sdb1 1 2 1 false 250000 abcdefghijklmnop 12350 {vm_ip_data['rollbaccineNum0']['private_ip']} 12340\" | sudo dmsetup create rollbaccine2"
@@ -81,16 +113,8 @@ def ssh_and_execute(public_ip, username, private_key_path, script_path, is_leade
     except Exception as e:
         print(f"Failed to connect to {public_ip}: {e}")
 
-vm_ip_data = {}
-
-# (The code for creating resource group, NSG, VNet, Subnet, and VMs is unchanged)
-
-# Save the data to a JSON file
-with open('vm_ips.json') as f:
-    vm_ip_data = json.load(f)
-
-
 for vm_name, ip_data in vm_ip_data.items():
     public_ip = ip_data['public_ip']
     is_leader = True if vm_name == 'rollbaccineNum0' else False
+    start_vm(vm_name)
     ssh_and_execute(public_ip, USERNAME, PRIVATE_KEY_PATH, SCRIPT_PATH, is_leader)
